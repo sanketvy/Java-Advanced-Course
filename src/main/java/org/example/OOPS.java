faed08b1dc4d0bb63f8a27c2038877a7caa2cf2c
Sanket Vyawahare <thundercapital.info@gmail.com>
2024-09-09 21:27:15 +0530

    initial repo setup

---
+++ src/main/java/org/example/OOPS.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class OOPS
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
